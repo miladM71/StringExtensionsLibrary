e5f97c980d94688930f4b48283f3d29e340ad6cd
miladnaza <https://github.com/miladM71/Lab2-backend>
2026-03-25 19:08:07 -0400

    Initial commit - class library

---
+++ StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace StringExtensionsLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUppercase(this string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return false;
+
+            return char.IsUpper(value[0]);
+        }
+    }
+}
